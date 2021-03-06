7a079d0a37f5ca00f7b8ff14189c609d2e623a96
John Bielejeski <jmbielejeski@gmail.com>
2021-03-06 12:06:32 -0700

    added button to go to addMovie page from movie list

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,32 +1,40 @@
 import React, { useEffect } from 'react';
+import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
 
 function MovieList() {
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+  let history = useHistory();
+  const dispatch = useDispatch();
+  const movies = useSelector(store => store.movies);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
+  useEffect(() => {
+      dispatch({ type: 'FETCH_MOVIES' });
+  }, []);
 
-    return (
-        <main>
-            <h1>Movie List</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
+  const goToAddMovie = () => {
+    console.log('in goToAddMovie')
+    history.push('/AddMovie');
+  }
 
-    );
+  return (
+      <main>
+          <h1>Movie List</h1>
+          <button onClick={goToAddMovie}>Add movie</button>
+          <section className="movies">
+              {movies.map(movie => {
+                  return (
+                      <div key={movie.id} >
+                          <h3>{movie.title}</h3>
+                          <img src={movie.poster} alt={movie.title}/>
+                      </div>
+                  );
+              })}
+          </section>
+      </main>
+
+  );
 }
 
 export default MovieList;
